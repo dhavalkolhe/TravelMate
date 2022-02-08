349790ebbaa0dfb5f80c3a2974c983d7b6d2da42
Kumari Anushka <anu.sb0340@gmail.com>
2022-02-09 03:26:57 +0530

    card loader removed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -75,7 +75,10 @@ function Card({
           sentRequests: arrayUnion(rideId)
         }).then(() => {
           setLoading(false);
-          setSendText("Request sent ✅");
+
+          if (!loading)
+            setSendText("Request sent ✅");
+
           setDisable(true);
 
         })
@@ -167,7 +170,7 @@ function Card({
           onClick={sendRequest}
         >
           {sendText}
-          {loading && <Loader size={20} />}
+          {/* {loading && <Loader size={10} />} */}
         </button>
       </div>
     </div>
